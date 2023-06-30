04bc121b599a27bb5794ca3ed43d361774dcb4cd
Elio CG <elcugo@gmail.com>
2023-06-30 01:50:37 -0600

    Added neogit community plugin.

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.cmake" },
 }
